6e6c434cb37b63e68fdff3eac61b41ff6ed727da
Sanchay Harneja <sanchay.h@gmail.com>
2012-07-03 17:43:20 +0530

    * Change package in scripts and properties file * Remove older java file

--- scripts/excel_cmp.bat
+++ scripts/excel_cmp.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SETLOCAL
 set dirname=%~dp0
-java -ea -Xmx512m -cp "%dirname%\dist\*;" com.ka.util.ExcelCompare %*
+java -ea -Xmx512m -cp "%dirname%\dist\*;" com.ka.excelcmp.ExcelCompare %*
 ENDLOCAL
