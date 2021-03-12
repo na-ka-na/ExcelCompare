cfbf018af799cb76332537059e92605e8cd1ea85
Ross Patterson <ross.patterson@gmail.com>
2021-03-12 16:09:01 -0500

    Use the correct way to detect Windows, and report which set of result files are in use for tests.

--- test/com/ka/spreadsheet/diff/SpreadSheetDifferSmokeTest.java
+++ test/com/ka/spreadsheet/diff/SpreadSheetDifferSmokeTest.java
@@ -12,9 +12,10 @@ public class SpreadSheetDifferSmokeTest {
 
   private static final File TEMP_DIR = new File("test/resources");
 
-  private static final boolean isWindows = "\\".equals(System.getProperty("file.separator"));
+  private static final boolean isWindows = (System.getProperty("os.name").startsWith("Windows"));
 
   public static void main(String[] args) throws Exception {
+    System.out.println("Using " + (isWindows ? "" : "non-") + "Windows expected results files.");
     testDiff(
         "Identical xlsx files",
         new String[] {"test/resources/ss1.xlsx", "test/resources/ss1.xlsx"},
@@ -131,7 +132,6 @@ public class SpreadSheetDifferSmokeTest {
   private static File resultFile(String resultFile) {
     return new File(isWindows ? ("win_" + resultFile) : resultFile);
   }
-
   public static void testDiff(String testName, String[] args, @Nullable File expectedOutFile,
       @Nullable File expectedErrFile) throws Exception {
     System.err.print(testName + "... ");
